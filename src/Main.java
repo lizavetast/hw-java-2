273e339dddc1baee80f3cb12e0273fb5c88cf294
Elizaveta Mishina <test.lizaveta@gmail.com>
2023-01-01 20:36:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100; //сумма на счету
+        int add = 2000; //сумма пополнения
+
+        int bonus = add / 100;
+        if (add > 1000) {
+            bonus = bonus;
+        } else
+            if (add < 1000)
+            { bonus = 0;
+        }
+
+        int total = account + add + bonus;
+
+        System.out.print("Полученно бонусов:" + bonus +
+                "Итоговый счёт:" + total);
+    }
+}
